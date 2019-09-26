cbd74ebfb6425d7a925016ad6f192c87941800ba
Aziskomara <55784203+Aziskomara@users.noreply.github.com>
2019-09-26 15:33:19 +0700

    Add files via upload

---
+++ Latihan9.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan9;
+
+/**
+ * Nama     : Azis komara
+ * Kelas    : IF-1
+ * Nim      : 10118029
+ * deskiripsi program   : program Text
+ * 
+ * @author Azis komara
+ */
+public class Latihan9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       char c;
+       char tab = '\t';
+       char newline = '\n';
+       c = 'A';
+        System.out.println("Hasil" + newline + "char" + tab + "berisi" + c);
+       
+    }
+    
+}
